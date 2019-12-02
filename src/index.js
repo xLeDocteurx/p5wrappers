4a2d434fbdf58c519de50aac1834b8787f3bbe7c
xLeDocteurx <lenoirc.fr@gmail.com>
2019-12-02 23:54:29 +0100

    first build

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import ReactP5Wrapper from './ReactP5Wrapper'
-import VueP5Wrapper from './VueP5Wrapper'
+import ReactP5Wrapper from './ReactP5Wrapper.js'
+import VueP5Wrapper from './VueP5Wrapper.vue'
 
 export default {
     ReactP5Wrapper,
